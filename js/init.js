839415c6186d1f23d60d4e5c88c3080e4a0c3c55
Juan Sebastian Camargo Visbal <juanscv@outlook.com>
2015-08-19 22:55:45 -0500

    Switched layout
    Added sidebar and removed parallax header.

--- js/init.js
+++ js/init.js
@@ -1,9 +1,15 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav();
+    $('.button-collapse').sideNav()({
+        menuWidth: 200, // Default is 240
+        edge: 'right', // Choose the horizontal origin
+        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      }
+    );
     $('.parallax').parallax();
     $('ul.tabs').tabs();
+    //$('.collapsible').collapsible();
 
-  }); // end of document ready  
+  }); // end of document ready
 })(jQuery); // end of jQuery name space
