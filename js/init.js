ecb4111be9fa60fef075cb1592ee79c92a7d77b7
Juan Sebastian Camargo Visbal <juanscv@outlook.com>
2015-08-27 21:08:17 -0500

    Revert "Switched layout"
    This reverts commit 839415c6186d1f23d60d4e5c88c3080e4a0c3c55.

--- js/init.js
+++ js/init.js
@@ -1,15 +1,9 @@
 (function($){
   $(function(){
 
-    $('.button-collapse').sideNav()({
-        menuWidth: 200, // Default is 240
-        edge: 'right', // Choose the horizontal origin
-        closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
-      }
-    );
+    $('.button-collapse').sideNav();
     $('.parallax').parallax();
     $('ul.tabs').tabs();
-    //$('.collapsible').collapsible();
 
-  }); // end of document ready
+  }); // end of document ready  
 })(jQuery); // end of jQuery name space
